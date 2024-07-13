a7f02b5a73ef7d2e8bcdbc6409c300aaa925d82c
Ankur Kumar <ankuralpha404@gmail.com>
2024-07-13 22:44:22 +0530

    complete till making a proper frame

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 This is just an experiment ground
 where basically I will do anything and experiment with this repo a lot,
 as much as I can .
+
+
+### Advices are welcomed
+If you are a person who want to suggest me then feel free to do that.
+Any thing that you want to recommend , like :
+    - some free to use resources
+    - improvement in code
+    - a better way to implement something
+    - or any other thing
+
--- main.cpp
+++ main.cpp
@@ -5,22 +5,36 @@ void printer(vector<string>& vec) ;
 int main() {
     cout << "Welcome to my tic tac toe game " << endl ;
     cout << "This game is made without using any copilot" << endl << endl;
+    string opt , comp; // define important elements
+    vector<string> vec;
+    while (true) {
     cout << "Enter what you would like to choose ?? " << endl << "\t1. X \n\t2. O" << endl;
     cout << "Type 1 or 2 " << endl;
-    string opt , comp;
     cin >> opt ;
-    while (true) {
         if ( opt == "1" ) {
             comp = "O";
             opt = "X" ;
+            break;
         }else if ( opt == "2" ) {
             comp = "X";
             opt = "O";
+            break;
         } else {
-            cout << "The value is incorrect" ;
+            cout << "The value is incorrect" << endl;
+            cout << "please re-enter " << endl;
         }
     }
-
+    printer(vec) ;
 }
 void printer(vector<string>& vec) {
+    cout << endl << endl ;
+    string blank = " ";
+    cout << "Y   *---*---*---*" << endl;
+    cout << "3   | "<< blank << " | "<< blank << " | "<< blank << blank << "|" << endl;
+    cout << "|   *---*---*---*" << endl ;
+    cout << "2   | "<< blank << " | "<< blank << " | "<< blank << blank << "|" << endl;
+    cout << "|   *---*---*---*" << endl ;
+    cout << "1   | "<< blank << " | "<< blank << " | "<< blank << blank << "|" << endl;
+    cout << "|   *---*---*---*" << endl ;
+    cout << "0-----1---2---3-X" << endl ;
 }
