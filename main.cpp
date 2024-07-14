a132e40a1921143e615a5eb4e7aa839d98beb2e3
Ankur Kumar <ankuralpha404@gmail.com>
2024-07-14 14:33:31 +0530

    changing from vector to string

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
-#include <vector>
 using namespace std;
-void printer(vector<string>& vec) ;
+void printer(string& str) ;
+void user_i(string& str, int i , int j );
+void com_i(string& str) ;
 int main() {
     cout << "Welcome to my tic tac toe game " << endl ;
     cout << "This game is made without using any copilot" << endl << endl;
     string opt , comp; // define important elements
-    vector<string> vec;
+    string str[3][3] = {
+        {" ", " " , " " },
+        {" ", " " , " " },
+        {" ", " " , " " }
+    };
     while (true) {
-    cout << "Enter what you would like to choose ?? " << endl << "\t1. X \n\t2. O" << endl;
-    cout << "Type 1 or 2 " << endl;
-    cin >> opt ;
+        cout << "Enter what you would like to choose ?? " << endl << "\t1. X \n\t2. O" << endl;
+        cout << "Type 1 or 2 " << endl;
+        cin >> opt ;
         if ( opt == "1" ) {
             comp = "O";
             opt = "X" ;
@@ -24,9 +29,8 @@ int main() {
             cout << "please re-enter " << endl;
         }
     }
-    printer(vec) ;
 }
-void printer(vector<string>& vec) {
+void printer(string& str) {
     cout << endl << endl ;
     string blank = " ";
     cout << "Y   *---*---*---*" << endl;
@@ -37,4 +41,8 @@ void printer(vector<string>& vec) {
     cout << "1   | "<< blank << " | "<< blank << " | "<< blank << blank << "|" << endl;
     cout << "|   *---*---*---*" << endl ;
     cout << "0-----1---2---3-X" << endl ;
+}
+void user_i(string& str , int i , int j ){
+}
+void com_i(string& str ) {
 }
