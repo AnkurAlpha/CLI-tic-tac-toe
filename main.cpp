1e0b0294340723747b2b5bb1c97ca5a64920241c
Ankur Kumar <ankuralpha404@gmail.com>
2024-07-14 15:03:00 +0530

    nothing is woriking , need to rebrush C++

--- main.cpp
+++ main.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-void printer(string& str) ;
-void user_i(string& str, int i , int j );
-void com_i(string& str) ;
+void printer(vector<string>& vec) ;
 int main() {
     cout << "Welcome to my tic tac toe game " << endl ;
     cout << "This game is made without using any copilot" << endl << endl;
     string opt , comp; // define important elements
-    string str[3][3] = {
-        {" ", " " , " " },
-        {" ", " " , " " },
-        {" ", " " , " " }
-    };
+    vector<string> vec;
     while (true) {
-        cout << "Enter what you would like to choose ?? " << endl << "\t1. X \n\t2. O" << endl;
-        cout << "Type 1 or 2 " << endl;
-        cin >> opt ;
+    cout << "Enter what you would like to choose ?? " << endl << "\t1. X \n\t2. O" << endl;
+    cout << "Type 1 or 2 " << endl;
+    cin >> opt ;
         if ( opt == "1" ) {
             comp = "O";
             opt = "X" ;
@@ -29,8 +24,21 @@ int main() {
             cout << "please re-enter " << endl;
         }
     }
+    printer(vec) ;
+    while (true) {
+        cout << "Enter position x axis : " ;
+        int i ;
+        cin >> i ;
+        cout << "Enter position y axis : " ;
+        int j;
+        cin >> j ;
+        int pos = (3 * (j- 1) + (i-1)) ; // for position
+        vector<string>::iterator itr = (vec.begin() + pos) ;
+        *itr = "X";
+        break;
+    }
 }
-void printer(string& str) {
+void printer(const vector<string> & vec) {
     cout << endl << endl ;
     string blank = " ";
     cout << "Y   *---*---*---*" << endl;
@@ -38,11 +46,7 @@ void printer(string& str) {
     cout << "|   *---*---*---*" << endl ;
     cout << "2   | "<< blank << " | "<< blank << " | "<< blank << blank << "|" << endl;
     cout << "|   *---*---*---*" << endl ;
-    cout << "1   | "<< blank << " | "<< blank << " | "<< blank << blank << "|" << endl;
+    cout << "1   | "<< *vec.begin() << " | "<< *(vec.begin()+1) << " | "<< *(vec.begin() +2)  << " |" << endl;
     cout << "|   *---*---*---*" << endl ;
     cout << "0-----1---2---3-X" << endl ;
-}
-void user_i(string& str , int i , int j ){
-}
-void com_i(string& str ) {
 }
